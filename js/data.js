8c2f12b716dfad19b6847471b6b9ccccd95b49be
LeemanF <frank_leeman@hotmail.com>
2021-11-23 16:39:15 +0100

    Move NotenQuiz to GitHub

---
+++ js/data.js
@@ -0,0 +1,108 @@
+var musicDataBasis = 
+[
+    {
+        "name": "lageDo",
+        "sound": "assets/audio/lageDo.mp3",
+        "key": "C3"
+    },
+    {
+        "name": "lageRe",
+        "sound": "assets/audio/lageRe.mp3",
+        "key": "D3"
+    },
+    {
+        "name": "lageMi",
+        "sound": "assets/audio/lageMi.mp3",
+        "key": "E3"
+    },
+    {
+        "name": "lageFa",
+        "sound": "assets/audio/lageFa.mp3",
+        "key": "F3"
+   },
+    {
+        "name": "lageSol",
+        "sound": "assets/audio/lageSol.mp3",
+        "key": "G3",
+    },
+    {
+        "name": "lageLa",
+        "sound": "assets/audio/lageLa.mp3",
+        "key": "A3"
+    },
+    {
+        "name": "lageSi",
+        "sound": "assets/audio/lageSi.mp3",
+        "key": "B3"
+    },
+    {
+        "name": "do",
+        "sound": "assets/audio/do.mp3",
+        "key": "C4"
+    },
+    {
+        "name": "re",
+        "sound": "assets/audio/re.mp3",
+        "key": "D4"
+    },
+    {
+        "name": "mi",
+        "sound": "assets/audio/mi.mp3",
+        "key": "E4"
+    },
+    {
+        "name": "fa",
+        "sound": "assets/audio/fa.mp3",
+        "key": "F4"
+    },
+    {
+        "name": "sol",
+        "sound": "assets/audio/sol.mp3",
+        "key": "G4"
+    },
+    {
+        "name": "la",
+        "sound": "assets/audio/la.mp3",
+        "key": "A4"
+    },
+    {
+        "name": "si",
+        "sound": "assets/audio/si.mp3",
+        "key": "B4"
+    },
+    {
+        "name": "hogeDo",
+        "sound": "assets/audio/hogeDo.mp3",
+        "key": "C5"
+    },
+    {
+        "name": "hogeRe",
+        "sound": "assets/audio/hogeRe.mp3",
+        "key": "D5"
+    },
+    {
+        "name": "hogeMi",
+        "sound": "assets/audio/hogeMi.mp3",
+        "key": "E5"
+    },
+    {
+        "name": "hogeFa",
+        "sound": "assets/audio/hogeFa.mp3",
+        "key": "F5"
+    },
+    {
+        "name": "hogeSol",
+        "sound": "assets/audio/hogeSol.mp3",
+        "key": "G5"
+    },
+    {
+        "name": "hogeLa",
+        "sound": "assets/audio/hogeLa.mp3",
+        "key": "A5"
+    },
+    {
+        "name": "hogeSi",
+        "sound": "assets/audio/hogeSi.mp3",
+        "key": "B5"
+    }
+];
---
+++ js/tonen.js
@@ -0,0 +1,202 @@
+"use strict"
+
+window.addEventListener("load", Initialise);
+//window.addEventListener("keypress", CheckSoundByKeyPress);
+
+var randomSound;
+var rightSound;
+var wrongSound;
+var bodyOfDocument;
+var btnRandomGeluid;
+var btnHerhaalGeluid;
+var btnLuisteren;
+var cbxOctaaf3;
+var cbxOctaaf4;
+var cbxOctaaf5;
+var musicData;
+var quiz = false;
+
+function Initialise(){
+    bodyOfDocument = document.querySelector('body');
+    btnRandomGeluid = document.getElementById('randomGeluid');
+    btnHerhaalGeluid = document.getElementById('herhaalGeluid');
+    btnLuisteren = document.getElementById('luisteren');
+    cbxOctaaf3 = document.getElementById('octaaf3');
+    cbxOctaaf4 = document.getElementById('octaaf4');
+    cbxOctaaf5 = document.getElementById('octaaf5');
+    
+    rightSound = {
+        audio: new Audio("assets/audio/juist.mp3")
+    }
+    wrongSound = {
+        audio: new Audio("assets/audio/fout.mp3")
+    }
+
+    btnRandomGeluid.addEventListener('click', () => {AddButtons(), SelectRandomSound()});
+    btnHerhaalGeluid.addEventListener('click', () => {if(quiz == true) PlaySound(randomSound)});
+    btnLuisteren.addEventListener('click', () => {quiz = false, rightSound.audio.play(), document.title = "Tonen herkennen"});
+    cbxOctaaf3.addEventListener('change', () => {BuildMusicData(), AddButtons()});
+    cbxOctaaf4.addEventListener('change', () => {BuildMusicData(), AddButtons()});
+    cbxOctaaf5.addEventListener('change', () => {BuildMusicData(), AddButtons()});
+
+    BuildMusicData();
+    AddButtons();
+    btnRandomGeluid.focus();
+
+}
+
+function CheckSoundByKeyPress(e) {
+    switch(e.key) {
+        case "A":
+        case "a":
+            CheckAnswer("A");
+            break;
+        case "B":
+        case "b":
+            CheckAnswer("B");
+            break;
+        case "C":
+        case "c":
+            CheckAnswer("C");
+            break;
+        case "D":
+        case "d":
+            CheckAnswer("D");
+            break;
+        case "E":
+        case "e":
+            CheckAnswer("E");
+            break;
+        case "F":
+        case "f":
+            CheckAnswer("F");
+            break;
+        case "G":
+        case "g":
+            CheckAnswer("G");
+            break;
+        case "S":
+        case "s":
+            SelectRandomSound()
+            break;
+        case "R":
+        case "r":
+            PlaySound(randomSound);
+            break;
+        case "Enter":
+        case "Space":
+            break;
+        default:
+            CheckAnswer("X");
+    }
+}
+
+function SelectRandomSound(){
+    let random = Math.floor(Math.random() * musicData.length);
+    
+    randomSound = {
+        audio: new Audio(musicData[random].sound),
+        key: musicData[random].key,
+        name: musicData[random].name
+    }
+
+    PlaySound(randomSound);
+    document.title = `${musicData[random].key} - ${musicData[random].name}`
+    quiz = true;
+}
+
+function PlaySound(selectedSound) {
+    selectedSound.audio.loop = false;
+    selectedSound.audio.play();
+    //console.log(selectedSound);
+}
+
+function CheckAnswer(answerToCheck) {
+    if(answerToCheck == randomSound.key.substring(0,answerToCheck.length)) {
+        rightSound.audio.loop = false;
+        rightSound.audio.play();
+        btnRandomGeluid.focus();
+    }
+    else {
+        wrongSound.audio.loop = false;
+        wrongSound.audio.play();
+    }
+}
+
+function AddButtons() {
+    let btn;
+    let pTag;
+    let div1 = document.createElement('div');
+    let div2 = document.createElement('div');
+    let div3 = document.createElement('div');
+    let numberOfDiv;
+    let selectedSound;
+    
+    let notendiv = document.getElementById('noten');
+    notendiv.innerHTML = "";
+    
+    div1.className = 'noten';
+    div2.className = 'noten';
+    div3.className = 'noten';
+
+
+    for(let i = 0; i < musicData.length; i++) {
+        numberOfDiv = parseInt(i/7) +1;
+        
+        btn = document.createElement('button');
+        pTag = document.createElement('p');
+
+        pTag.innerHTML = `${musicData[i].key}<br>${musicData[i].name}`;
+
+        btn.appendChild(pTag);
+        btn.addEventListener('click', () => 
+            {
+                if(quiz == true) 
+                    CheckAnswer(musicData[i].key); 
+                else 
+                    {
+                        let selectedSound;
+                        selectedSound = {
+                            audio: new Audio(musicData[i].sound),
+                            key: musicData[i].key,
+                            name: musicData[i].name
+                        };
+                        PlaySound(selectedSound);
+                    }
+            });
+
+        if(numberOfDiv == 1) div1.appendChild(btn);
+        else if(numberOfDiv == 2) div2.appendChild(btn);
+        else if(numberOfDiv ==3) div3.appendChild(btn);
+    }
+    if(div1.childElementCount != 0) notendiv.appendChild(div1);
+    if(div2.childElementCount != 0) notendiv.appendChild(div2);
+    if(div3.childElementCount != 0) notendiv.appendChild(div3);
+}
+
+function BuildMusicData() {
+    musicData = musicDataBasis;
+    quiz = false;
+    document.title = "Tonen herkennen";
+
+    if(!cbxOctaaf3.checked) {
+        musicData = musicData.filter(a => a.key.substr(1,1) != 3);
+    }
+    if(!cbxOctaaf4.checked) {
+        musicData = musicData.filter(a => a.key.substr(1,1) != 4);
+    }
+    if(!cbxOctaaf5.checked) {
+        musicData = musicData.filter(a => a.key.substr(1,1) != 5);
+    }
+
+    if(!cbxOctaaf3.checked && !cbxOctaaf4.checked && !cbxOctaaf5.checked) {
+        btnRandomGeluid.style.display = "none";
+        btnHerhaalGeluid.style.display = "none";
+        btnLuisteren.style.display = "none";
+    }
+    else {
+        btnRandomGeluid.style.display = "inline-block";
+        btnHerhaalGeluid.style.display = "inline-block";
+        btnLuisteren.style.display = "inline-block";
+    }
+}
